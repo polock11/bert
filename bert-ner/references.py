282c4066218b408788836a1b0258934659cda666
Shakib Ibna Shameem Polock <50078050+polock11@users.noreply.github.com>
2024-11-30 00:41:12 +0200

    updated resources

---
+++ bert-ner/references.py
@@ -0,0 +1,4 @@
+# https://mccormickml.com/2019/07/22/BERT-fine-tuning/
+# https://github.com/NielsRogge/Transformers-Tutorials/blob/master/BERT/Custom_Named_Entity_Recognition_with_BERT.ipynb
+# https://colab.research.google.com/github/NielsRogge/Transformers-Tutorials/blob/master/BERT/Custom_Named_Entity_Recognition_with_BERT_only_first_wordpiece.ipynb#scrollTo=Eh3ckSO0YMZW 
+# https://huggingface.co/docs/transformers/en/tasks/token_classification 
